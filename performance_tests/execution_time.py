ce409da4775acda6ee9db120844084fca24cd8ef
catherinemeadows <meadowsc@gwu.edu>
2021-11-23 10:24:38 -0600

    add med functions to fibonacci script

--- performance_tests/execution_time.py
+++ performance_tests/execution_time.py
@@ -5,38 +5,45 @@ from statistics import mean
 
 def light_functions():
     # light function simple fibonacci
-    light_arr = []
+    light_list = []
     for i in range (0,5):
         start_time = time.time()
         os.system ("echo '1' | http :10000")
         finish_time = time.time() - start_time
-        light_arr.append(finish_time)
-    return light_arr
+        light_list.append(finish_time)
+    return light_list
 
-#def heavy_functions():
-    # metrics for heavy functions
- #   return # dict
+def med_functions():
+    # metrics for medium function
+    med_list = []
+    for i in range(0,5):
+        start_time = time.time()
+        os.system("echo '10' | http :10000")
+        finish_time = time.time() - start_time
+        med_list.append(finish_time)
+    return med_list
 
-
-def plot_all(light_arr):
+def plot_all(light_list, med_list):
     # get average to plot
-    light_avg = []
-    light_avg.append(mean(light_arr))
-    print(light_avg)
+    function_avgs = []
+    function_avgs.append(mean(light_list))
+    function_avgs.append(mean(med_list))
+
+    # plot 
     labels = []
-    labels.append("Fibonacci(1)")
-    plt.bar(labels, light_avg)
-    plt.title("Lightweight Functions")
-    plt.xlabel("Functions")
-    plt.ylabel("seconds")
+    labels.append("Fibonacci(1), Fibonacci(10)")
+    plt.bar(labels, function_avgs)
+    plt.title("Sledge - Fibonacci Performance")
+    plt.xlabel("Function Calls")
+    plt.ylabel("Seconds")
     plt.show()
-    plt.savefig('light_function.png')
+    plt.savefig('fibonacci_function.png')
 
 def main():
     print("Starting...")
-    light_arr = light_functions()
-    #heavy = heavy_functions()
-    plot_all(light_arr)
+    light_list = light_functions()
+    med_list = med_functions()
+    plot_all(light_list, med_list)
     print("Done!")
 
 if __name__ == "__main__":
